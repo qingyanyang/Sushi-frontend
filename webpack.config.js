14362e485a36a6e932c82898c811c154817455e6
a1865304 <yangqingyan0@gmail.com>
2023-09-18 14:35:09 +0930

    babel-config

--- webpack.config.js
+++ webpack.config.js
@@ -1,7 +1,28 @@
+const path = require('path');
+
 module.exports = {
+    entry: './src/index.js',
+    output: {
+        filename: '[name].bundle.js', 
+        path: path.resolve(__dirname, 'dist'),
+    },
+    module: {
+        rules: [
+            {
+                test: /\.(js|jsx)$/,
+                exclude: /node_modules/,
+                use: {
+                    loader: 'babel-loader',
+                    options: {
+                        presets: ['@babel/preset-env', '@babel/preset-react'],
+                    },
+                },
+            },
+        ],
+    },
     optimization: {
         splitChunks: {
-            chunks: 'all',
+            chunks: 'all', 
             cacheGroups: {
                 vendor: {
                     test: /[\\/]node_modules[\\/]/,
