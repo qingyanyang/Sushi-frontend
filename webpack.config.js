bbe880c4cb63507c40cd21edd263d8c68e4ca878
a1865304 <yangqingyan0@gmail.com>
2023-09-18 19:01:55 +0930

    config: web

--- webpack.config.js
+++ webpack.config.js
@@ -1,3 +1,4 @@
+const HtmlWebpackPlugin = require('html-webpack-plugin');
 const path = require('path');
 
 module.exports = {
@@ -49,4 +50,9 @@ module.exports = {
             },
         },
     },
+    plugins: [
+        new HtmlWebpackPlugin({
+            template: './src/index.html'
+        }),
+    ],
 };
